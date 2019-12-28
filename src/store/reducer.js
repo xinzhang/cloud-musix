cd7cd04ba03067e06832ce7e087d4a8765531ea4
Xin Zhang <xinzhang@Xins-MacBook-Pro.local>
2019-12-28 14:56:43 +1100

    add redux

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,6 @@
-//reducers.js
 import { combineReducers } from 'redux-immutable';
+import { reducer as recommendReducer } from '../application/Recommend/store/index';
 
 export default combineReducers ({
-// 之后开发具体功能模块的时候添加 reducer  
+  recommend: recommendReducer,
 });
